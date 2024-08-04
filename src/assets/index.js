b94047a9c9da4b808a0b38792cc1d887587b712a
unknown <emankamran83@gmail.com>
2024-08-04 18:28:37 +0500

    Updated my portfolio

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -21,12 +20,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import graphql from "./tech/graphql.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
 import algoryte from "./company/algoryte.png";
 import bytewise from "./company/bytewise.png";
-import tesla from "./company/tesla.png";
 
 import taskmt from "./taskmt.png";
 import loopstudio from "./loopstudio.png";
@@ -35,13 +30,9 @@ import sass from "./sass.png";
 import furnitureshop from "./furnitureshop.png";
 import weblog from "./weblog.png";
 import grocify from "./grocify.png";
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import weatherapp from "./weatherapp.png";
 import insta from "./insta.png";
 export {
-  logo,
   backend,
   creator,
   insta,
@@ -67,17 +58,10 @@ export {
   tailwind,
   typescript,
   graphql,
-  meta,
-  shopify,
   algoryte,
-  starbucks,
-  tesla,
   taskmt,
   furnitureshop,
   omnifood,
   loopstudio,
-  carrent,
-  jobit,
-  tripguide,
   bytewise,
 };
